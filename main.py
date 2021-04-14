d06d03e69e5875d159a5f5f4ea5ede125c5d07c5
hamed6 <h.ghodsinezhad@gmail.com>
2021-04-14 15:33:59 +0300

    read file

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import datetime  
-import openpyxl 
+import  openpyxl as opxl
+
 # print(datetime.date.today())
+# f=open('Book.xlsx','r')
+# print(f.read())
+# f.close()
 
 
-f=open('b.txt','r')
+wb=opxl.load_workbook('Book.xlsx')
+ws=wb['Sheet1']
+# c=ws['A1':'A4']
+# print(wb['A1'.value])
 
-print(f.read())
-f.close()
+for row in ws.iter_rows():
+    for cell in row:
+        print(cell.value)
